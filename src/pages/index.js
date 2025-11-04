25819f8e4e1edf48fefb0a3755d9964c730772f1
Gabriel Moreira <gabrielmoreira7301@gmail.com>
2025-11-04 18:04:25 +0000

    login card(will design background once app design is down)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import AdicionarNomes from '../components/AdicionarNomes';
-import VerNomes from '../components/VerNomes';
+import React from "react";
+import Form from "../components/LogInForm";
+
+
 
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen p-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 max-w-7xl mx-auto">
-        <VerNomes />
-        <AdicionarNomes />
+      <div className="flex justify-center items-center min-h-screen">
+          <Form />
       </div>
     </div>
   );
